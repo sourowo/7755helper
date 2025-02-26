c0978232ef61218820ea7fe8346e74c3f9c1cb25
sourowo <a0917060088@gmail.com>
2025-02-26 18:10:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -40,11 +40,20 @@ function createCustomOptionButton(optionText) {
     var newButton = document.createElement("button");
     newButton.className = "option-btn";
     newButton.textContent = optionText;
+
+    // 點擊按鈕後將該選項文字輸入到輸入框
     newButton.onclick = function () {
-        setInputValue(optionText);
+        setInputValue(optionText);  // 將選項文字加入到輸入框
     };
 
     customOptionsContainer.appendChild(newButton);
+}
+
+// 設定輸入框的值
+function setInputValue(option) {
+    var inputField = document.getElementById("input-field");
+    inputField.value += option + "\n";  // 將選項加入輸入框並換行
+    saveInputValue();  // 保存輸入框內容
 }
 
 // 載入頁面時加載自定義選項
